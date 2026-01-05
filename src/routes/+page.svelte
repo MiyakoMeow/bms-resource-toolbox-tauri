824f7cb8a24e5098e6e5ea90d7d5bffeaf928dd2
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2026-01-05 14:59:47 +0800

    feat: prettier-tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,40 +11,40 @@ async function greet(event: Event) {
 }
 </script>
 
-<main class="m-0 pt-[10vh] flex flex-col justify-center text-center">
+<main class="m-0 flex flex-col justify-center pt-[10vh] text-center">
   <h1 class="text-center">Welcome to Tauri + Svelte</h1>
 
   <div class="flex justify-center">
     <a
       href="https://vite.dev"
       target="_blank"
-      class="font-medium text-[#646cff] hover:text-[#535bf2] dark:hover:text-[#24c8db] no-underline"
+      class="font-medium text-[#646cff] no-underline hover:text-[#535bf2] dark:hover:text-[#24c8db]"
     >
       <img
         src="/vite.svg"
-        class="h-24 p-6 will-change-filter transition-[0.75s] hover:drop-shadow-[0_0_2em_#747bff]"
+        class="will-change-filter h-24 p-6 transition-[0.75s] hover:drop-shadow-[0_0_2em_#747bff]"
         alt="Vite Logo"
       />
     </a>
     <a
       href="https://tauri.app"
       target="_blank"
-      class="font-medium text-[#646cff] hover:text-[#535bf2] dark:hover:text-[#24c8db] no-underline"
+      class="font-medium text-[#646cff] no-underline hover:text-[#535bf2] dark:hover:text-[#24c8db]"
     >
       <img
         src="/tauri.svg"
-        class="h-24 p-6 will-change-filter transition-[0.75s] hover:drop-shadow-[0_0_2em_#24c8db]"
+        class="will-change-filter h-24 p-6 transition-[0.75s] hover:drop-shadow-[0_0_2em_#24c8db]"
         alt="Tauri Logo"
       />
     </a>
     <a
       href="https://svelte.dev"
       target="_blank"
-      class="font-medium text-[#646cff] hover:text-[#535bf2] dark:hover:text-[#24c8db] no-underline"
+      class="font-medium text-[#646cff] no-underline hover:text-[#535bf2] dark:hover:text-[#24c8db]"
     >
       <img
         src="/svelte.svg"
-        class="h-24 p-6 will-change-filter transition-[0.75s] hover:drop-shadow-[0_0_2em_#ff3e00]"
+        class="will-change-filter h-24 p-6 transition-[0.75s] hover:drop-shadow-[0_0_2em_#ff3e00]"
         alt="SvelteKit Logo"
       />
     </a>
@@ -56,11 +56,11 @@ async function greet(event: Event) {
       id="greet-input"
       placeholder="Enter a name..."
       bind:value={name}
-      class="mr-[5px] rounded-lg border border-transparent px-[1.2em] py-[0.6em] text-base font-medium font-inherit text-[#0f0f0f] bg-white transition-[border-color_0.25s] shadow-[0_2px_2px_rgba(0,0,0,0.2)] outline-none dark:text-white dark:bg-[#0f0f0f98]"
+      class="font-inherit mr-[5px] rounded-lg border border-transparent bg-white px-[1.2em] py-[0.6em] text-base font-medium text-[#0f0f0f] shadow-[0_2px_2px_rgba(0,0,0,0.2)] transition-[border-color_0.25s] outline-none dark:bg-[#0f0f0f98] dark:text-white"
     />
     <button
       type="submit"
-      class="rounded-lg border border-transparent px-[1.2em] py-[0.6em] text-base font-medium font-inherit text-[#0f0f0f] bg-white transition-[border-color_0.25s] shadow-[0_2px_2px_rgba(0,0,0,0.2)] outline-none cursor-pointer hover:border-[#396cd8] active:border-[#396cd8] active:bg-[#e8e8e8] dark:text-white dark:bg-[#0f0f0f98] dark:active:bg-[#0f0f0f69]"
+      class="font-inherit cursor-pointer rounded-lg border border-transparent bg-white px-[1.2em] py-[0.6em] text-base font-medium text-[#0f0f0f] shadow-[0_2px_2px_rgba(0,0,0,0.2)] transition-[border-color_0.25s] outline-none hover:border-[#396cd8] active:border-[#396cd8] active:bg-[#e8e8e8] dark:bg-[#0f0f0f98] dark:text-white dark:active:bg-[#0f0f0f69]"
     >
       Greet
     </button>
